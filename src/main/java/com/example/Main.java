acf558acad1db775e801636d6885033ea9ed0d44
Tony Bowen <wbowen30@gatech.edu>
2021-01-08 16:25:50 -0500

    qwerasdF

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -60,7 +60,7 @@ public class Main extends ListenerAdapter{
 
   public static void main(String[] args) throws Exception {
     SpringApplication.run(Main.class, args);
-    JDABuilder.createLight("", GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
+    JDABuilder.createLight("Nzk2NTIxNTE3Mzk5NjcwODA0.X_ZIeA.iNHyE3NsSRQB9VxX1_Eb_7qC6Cg", GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
             .addEventListeners(new Main())
             .build();
     Timer timer = new Timer();
@@ -83,14 +83,64 @@ public class Main extends ListenerAdapter{
     }
     else if(messageTest.contains("findporn"))
     {
-      {
+      JDA JDA = channel.getJDA();
+      OkHttpClient http = JDA.getHttpClient();
+      EmbedBuilder result= new EmbedBuilder();
+
+      Integer searchValue = (int) (((Math.random()*(30000 - 1))) + 1);
+
+      okhttp3.Request request = new Request.Builder().url("https://cdn.porngifs.com/img/" + searchValue).build();
+      Response response = null;
+      try {
+        response = http.newCall(request).execute();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      try {
+        response = http.newCall(request).execute();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      try {
+        InputStream body = response.body().byteStream();
+        result.setImage("attachment://image.gif"); // Use same file name from attachment
+        Response finalResponse = response;
+        Response finalResponse1 = response;
+        channel.sendMessage(result.build())
+                .addFile(body, "image.gif") // Specify file name as "image.png" for embed (this must be the same, its a reference which attachment belongs to which image in the embed)
+                .queue(m -> finalResponse.close(), error -> { // Send message and close response when done
+                  finalResponse1.close();
+                  RestAction.getDefaultFailure().accept(error);
+                });
+      } catch (Throwable ex) {
+        response.close();
+        if (ex instanceof Error) throw (Error) ex;
+        else throw (RuntimeException) ex;
+      }
+    }
+    else if(messageTest.contains("findgif"))
+    {
+
+      String gifSearch = messageTest;
+      gifSearch = gifSearch.replace("findgif ","");
+
+      Giphy giphy = new Giphy("4SoqP1X0f38P3FRthyRe97l7f8Vnd51q");
+
+      channel.sendMessage("Searching giphy for " + gifSearch) /* => RestAction<Message> */
+              .queue();
+
+
+      SearchFeed feed = null;
+      try {
+        feed = giphy.search(gifSearch, 1, (int) (((Math.random()*(20 - 1))) + 1));
+      } catch (GiphyException e) {
+        e.printStackTrace();
+      }
         JDA JDA = channel.getJDA();
         OkHttpClient http = JDA.getHttpClient();
         EmbedBuilder result= new EmbedBuilder();
 
-        Integer searchValue = (int) (((Math.random()*(30000 - 1))) + 1);
-
-        okhttp3.Request request = new Request.Builder().url("https://cdn.porngifs.com/img/" + searchValue).build();
+      okhttp3.Request request = new Request.Builder().url(feed.getDataList().get(0).getImages().getOriginal().getUrl()).build();
         Response response = null;
         try {
           response = http.newCall(request).execute();
@@ -118,8 +168,101 @@ public class Main extends ListenerAdapter{
           if (ex instanceof Error) throw (Error) ex;
           else throw (RuntimeException) ex;
         }
+    }
+    else if(messageTest.contains("flipacoin heads") && !(author.getName().equals("Mr. roBOT")))
+    {
+      int temp = (Math.random() <= 0.5) ? 1 : 2;
+      //Heads = 1, Tails = 2
+      if(temp == 1){
+        channel.sendMessage("It was Heads! Gain some money Boi") /* => RestAction<Message> */
+                .queue();
+        int value = 0;
+        if(gameMap.containsKey(author.getName()))
+        {
+          value = gameMap.get(author.getName());
+          gameMap.replace(author.getName(), (value+5));
+        }
+      }
+      else {
+        channel.sendMessage("It was Tails idiot lose some money") /* => RestAction<Message> */
+                .queue();
+        int value = 0;
+        if(gameMap.containsKey(author.getName()))
+        {
+          value = gameMap.get(author.getName());
+          gameMap.replace(author.getName(), (value-5));
+        }
       }
     }
+    else if(messageTest.contains("flipacoin tails") && !(author.getName().equals("Mr. roBOT")))
+    {
+      int temp = (Math.random() <= 0.5) ? 1 : 2;
+      //Heads = 1, Tails = 2
+      if(temp == 2){
+        channel.sendMessage("It was Tails! Gain some money Boi") /* => RestAction<Message> */
+                .queue();
+        int value = 0;
+        if(gameMap.containsKey(author.getName()))
+        {
+          value = gameMap.get(author.getName());
+          gameMap.replace(author.getName(), (value+5));
+        }
+      }
+      else {
+        channel.sendMessage("It was Heads idiot lose some money") /* => RestAction<Message> */
+                .queue();
+        int value = 0;
+        if(gameMap.containsKey(author.getName()))
+        {
+          value = gameMap.get(author.getName());
+          gameMap.replace(author.getName(), (value-5));
+        }
+      }
+    }
+    else if(messageTest.contains("losesomemoney"))
+    {
+      int value = 0;
+      if(gameMap.containsKey(author.getName()))
+      {
+        value = gameMap.get(author.getName());
+        gameMap.replace(author.getName(), (value-5));
+      }
+    }
+    else if(messageTest.contains("gainsomemoney"))
+    {
+      int value = 0;
+      if(gameMap.containsKey(author.getName()))
+      {
+        value = gameMap.get(author.getName());
+        gameMap.replace(author.getName(), (value+5));
+      }
+    }
+    else if ((messageTest.contains("playsomegame") || messageTest.contains("iwannaplay") || messageTest.contains("startsomegame")) && !(author.getName().equals("Mr. roBOT"))) {
+      if (messageTest.contains("playsomegame")) {
+        channel.sendMessage("Lets play a game! Type iwannaplay to participate and startsomegame to start") /* => RestAction<Message> */
+                .queue();
+      }
+
+      if (messageTest.contains("iwannaplay")) {
+        gameMap.put(author.getName(), 50);
+        channel.sendMessage("Fuck you " + author.getName() + ", but I added your ass to the game anyway.") /* => RestAction<Message> */
+                .queue();
+      }
+      if (messageTest.contains("startsomegame")) {
+        channel.sendMessage("somegame Participants and $:") /* => RestAction<Message> */
+                .queue();
+        Iterator it = gameMap.entrySet().iterator();
+        while (it.hasNext()) {
+          Map.Entry obj = (Map.Entry) it.next();
+          channel.sendMessage(obj.getKey() + ": $" + obj.getValue()) /* => RestAction<Message> */
+                  .queue();
+          }
+
+        channel.sendMessage("Type flipacoin followed by heads or tails (i.e. flipacoin heads)") /* => RestAction<Message> */
+                .queue();
+        }
+      }
+
     else if (author.getName().equals("Carlos Pascetti")){
       if(jasonCounter < 5)
       {
